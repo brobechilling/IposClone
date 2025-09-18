00341c3caae106deab4dc26327d47435e229ec3e
Quoc Nguyen <quocnguyen112505@gmail.com>
2025-09-18 16:10:01 +0700

    test dev branch

--- src/main/java/com/helloworld/demo/DemoApplication.java
+++ src/main/java/com/helloworld/demo/DemoApplication.java
@@ -15,6 +15,6 @@ public class DemoApplication {
 
 	@GetMapping("/")
 	public String home() {
-        return "Hello World - test deploy wtf - cicd works ??? - test clear unused image";
+        return "Hello World - test deploy wtf - cicd works ??? - test clear unused image -- update branch ci cd ???";
     }
 }
